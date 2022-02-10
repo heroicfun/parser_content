bdca6a56483abe1c49f008a16744fa789bec0554
alexbprofit2 <98819455+alexbprofit2@users.noreply.github.com>
2022-02-10 14:58:12 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
 import telebot
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from elasticsearch import Elasticsearch as ES
+
+import requests, json
 
 bot = telebot.TeleBot('5208795479:AAG9a9sa3hsU8_L2PNn5h1WsznjhmTBl9Mc')
 
 from selenium.webdriver.chrome.options import Options
-
+import shelve
 
 def search(m, dicts):
     return [value for key, value in dicts.items() if m in key]
@@ -15,7 +16,6 @@ def search(m, dicts):
 @bot.message_handler(commands=['start'])
 def send_start_msg(m):
     bot.send_message(m.chat.id, "hello, enter a search query")
-
 
 @bot.message_handler(content_types=['text'])
 def send_user_message(m):
@@ -43,19 +43,25 @@ def send_user_message(m):
         elem = driver.find_element_by_xpath(
             f'/html/body/div[1]/div[1]/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[1]/article[{i}]/div[1]/h2/a/span').text
 
-        names.append(elem)
+        names.append(elem.lower())
+    shelf = shelve.open('database', 'n')
 
     dictionary = dict(zip(names, links))
+    for key, value in dictionary.items():
+        shelf[key] = value
 
     result = m.text
 
     parts = result.split(" ")
-
+    final_set = []
     for part in parts:
-        res = search(part, dictionary)
+        res = search(part, shelf)
         print(res)
         for item in res:
-            bot.send_message(m.chat.id, item)
-
+            if item not in final_set:
+                final_set.append(item)
+    for item in final_set:
+        bot.send_message(m.chat.id, item)
+    shelf.close()
 
 bot.polling(none_stop=True, interval=0)
