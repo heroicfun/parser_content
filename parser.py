9078bca5e13b1a64bbd53398f743442f074990c2
alexbprofit2 <98819455+alexbprofit2@users.noreply.github.com>
2022-02-14 16:36:12 +0200

    add url shortener

--- parser.py
+++ parser.py
@@ -1,13 +1,14 @@
-import telebot
+import time
+
+import telebot, pyshorteners
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
-import requests, json
 
 bot = telebot.TeleBot('5208795479:AAG9a9sa3hsU8_L2PNn5h1WsznjhmTBl9Mc')
 
 from selenium.webdriver.chrome.options import Options
 import shelve
+
 
 def search(m, dicts):
     return [value for key, value in dicts.items() if m in key]
@@ -16,6 +17,10 @@ def search(m, dicts):
 @bot.message_handler(commands=['start'])
 def send_start_msg(m):
     bot.send_message(m.chat.id, "hello, enter a search query")
+
+
+shorter = pyshorteners.Shortener()
+
 
 @bot.message_handler(content_types=['text'])
 def send_user_message(m):
@@ -28,7 +33,7 @@ def send_user_message(m):
     driver.get('https://habr.com/ru/flows/develop/')
 
     links = []
-
+    medium = [[], []]
     for i in range(20):
         try:
             elem = driver.find_element_by_xpath(
@@ -44,11 +49,32 @@ def send_user_message(m):
             f'/html/body/div[1]/div[1]/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[1]/article[{i}]/div[1]/h2/a/span').text
 
         names.append(elem.lower())
-    shelf = shelve.open('database', 'n')
+    driver.get('https://medium.com')
+    time.sleep(5)
+    for i in range(30):
+        try:
+            elem = driver.find_element_by_xpath(
+                f'/html/body/div/div/div[4]/div[3]/div/div/div/div[1]/div/div/div[1]/div[{i + 1}]/div/div/div/a')
+            medium[0].append(elem.get_attribute('href'))
+        except:
+            continue
+
+    for i in range(1, len(medium[0]) + 1):
+        elem = driver.find_element_by_xpath(
+            f'/html/body/div/div/div[4]/div[3]/div/div/div/div[1]/div/div/div[1]/div[{i}]/div/div/div/a/h2').text
+        medium[1].append(elem.lower())
+
+    shelf = shelve.open('database')
 
     dictionary = dict(zip(names, links))
+
+    medium_dict = dict(zip(medium[1], medium[0]))
     for key, value in dictionary.items():
-        shelf[key] = value
+        if key not in shelf.keys():
+            shelf[key] = value
+    for key, value in medium_dict.items():
+        if key not in shelf.keys():
+            shelf[key] = value
 
     result = m.text
 
@@ -61,7 +87,12 @@ def send_user_message(m):
             if item not in final_set:
                 final_set.append(item)
     for item in final_set:
-        bot.send_message(m.chat.id, item)
+        if 'tinyurl' not in item:
+            bot.send_message(m.chat.id, shorter.tinyurl.short(item))
+        else:
+            bot.send_message(m.chat.id, item)
     shelf.close()
 
+
 bot.polling(none_stop=True, interval=0)
+
