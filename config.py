943f563726de5956e189027781539b2441626029
alexbprofit2 <98819455+alexbprofit2@users.noreply.github.com>
2022-02-21 16:28:41 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+class Settings:
+    helper = {
+        'habr': {
+            'site_link': 'https://habr.com/ru/flows/develop/',
+            'xpath_link': '/html/body/div[1]/div[1]/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[1]/article/div[1]/h2/a',
+            'xpath_name': '/html/body/div[1]/div[1]/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[1]/article/div[1]/h2/a/span'
+        },
+        'medium': {
+            'site_link': 'https://medium.com',
+            'xpath_link': '/html/body/div/div/div[4]/div[3]/div/div/div/div[1]/div/div/div[1]/div/div/div/div/a',
+            'xpath_name': '/html/body/div/div/div[4]/div[3]/div/div/div/div[1]/div/div/div[1]/div/div/div/div/a/h2'
+        }
+    }
