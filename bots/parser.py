8cc1d56519f08bf782bf2d3900f714f97588efa0
alexbprofit2 <98819455+alexbprofit2@users.noreply.github.com>
2022-02-21 16:29:11 +0200

    Update parser.py

--- bots/parser.py
+++ bots/parser.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-
+from config import Settings
 from bots import db
 
 
@@ -24,18 +24,7 @@ def parse():
     chrome_options = driver_init()
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
-    helper = {
-        'habr': {
-            'site_link': 'https://habr.com/ru/flows/develop/',
-            'xpath_link': '/html/body/div[1]/div[1]/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[1]/article/div[1]/h2/a',
-            'xpath_name': '/html/body/div[1]/div[1]/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[1]/article/div[1]/h2/a/span'
-        },
-        'medium': {
-            'site_link': 'https://medium.com',
-            'xpath_link': '/html/body/div/div/div[4]/div[3]/div/div/div/div[1]/div/div/div[1]/div/div/div/div/a',
-            'xpath_name': '/html/body/div/div/div[4]/div[3]/div/div/div/div[1]/div/div/div[1]/div/div/div/div/a/h2'
-        }
-    }
+    helper = Settings.helper
 
     parsed_links = []
     parsed_names = []
